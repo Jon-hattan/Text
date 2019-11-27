687930e423e1c2e1684ce4189fe76e07480e5e0b
Jon-hattan <58208209+Jon-hattan@users.noreply.github.com>
2019-11-27 16:11:23 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/okyux5qklhy8jye/language.pkl?dl=1'
-export_file_name = 'language.pkl'
+export_file_url = 'https://www.dropbox.com/s/3utm6fza5p1qy35/newlanguage.pkl?dl=1'
+export_file_name = 'newlanguage.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
